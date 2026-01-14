f3be5741520b9335447c21b452810ccc0217c0d5
jarojas <jalberto.roj@gmail.com>
2026-01-14 03:42:58 -0600

    tweak attributes and logic for idle animation

--- src/main/java/com/jarojas/world/entity/custom/OmnidroidV0Entity.java
+++ src/main/java/com/jarojas/world/entity/custom/OmnidroidV0Entity.java
@@ -1,6 +1,7 @@
 package com.jarojas.world.entity.custom;
 
-import net.minecraft.block.entity.ShulkerBoxBlockEntity.AnimationStage;
+import net.minecraft.entity.AnimationState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
@@ -9,8 +10,8 @@ import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.world.World;
 
 public class OmnidroidV0Entity extends OmnidroidBase {
-     
 
+    public final AnimationState idleAnimationState = new AnimationState();
 
     public OmnidroidV0Entity(EntityType<? extends HostileEntity> entityType, World world) {
         super(entityType, world);
@@ -19,23 +20,29 @@ public class OmnidroidV0Entity extends OmnidroidBase {
     public static DefaultAttributeContainer.Builder createOmnidroidV0Attributes() {
         return MobEntity.createMobAttributes()
                 .add(EntityAttributes.MAX_HEALTH, 20.0)
-                .add(EntityAttributes.MOVEMENT_SPEED, 0.45)
-                .add(EntityAttributes.ARMOR, 2.0)
-                .add(EntityAttributes.FOLLOW_RANGE, 4.0);
-    }
-
-    private void setUpAnimationStates() {
-        // Placeholder for animation state setup
+                .add(EntityAttributes.ATTACK_DAMAGE, 3)
+                .add(EntityAttributes.MOVEMENT_SPEED, 0.25)
+                .add(EntityAttributes.ARMOR, 1.0)
+                .add(EntityAttributes.FOLLOW_RANGE, 8.0); //matching with LookAtEntityGoal's range
     }
 
     @Override
     public void tick() {
-        // TODO Auto-generated method stub
         super.tick();
+
+        updateAnimations();
     }
 
-    AnimationStage getAnimationStage() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    private void updateAnimations() {
+        if (!this.isMovingHorizontally()) {
+            this.idleAnimationState.startIfNotRunning(this.age);
+        } else {
+            this.idleAnimationState.stop();
+        }
+	}
+
+    @Override
+	protected Entity.MoveEffect getMoveEffect() {
+		return Entity.MoveEffect.NONE;
+	}
  }
